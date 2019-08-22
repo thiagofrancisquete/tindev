7e498bc11737ae1c11acc9b2c7ca05a057ae72bd
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-08-22 15:28:11 -0300

    rota likes, dislikes, litagem de devs

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController')
 
 const routes = express.Router();
 
+// rota de listagem dos devs
+routes.get('/devs', DevController.index);
+
 // criar um novo dev
 routes.post('/devs', DevController.store);
+
+// rota de likes e dislikes
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
